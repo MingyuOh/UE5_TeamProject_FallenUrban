7ddb091314d178a162f934516382f8b526677f44
MingyuOh <hotalsrb12@naver.com>
2023-01-31 21:23:18 +0900

    Push project file
    First Commit!

---
+++ Source/FallenUrban/FallenUrbanGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FallenUrbanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FALLENURBAN_API AFallenUrbanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
